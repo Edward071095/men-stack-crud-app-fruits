8d29ebbf7157c1881143f03997c989e815355a3a
EdwardW710 <EdwardW710@yahoo.com>
2025-02-14 12:43:42 -0500

    configure app for mongoDB connection

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express = require('express');
+const dotenv = require("dotenv"); // require package
+dotenv.config(); // Loads the environment variables from .env file
+const express = require("express");
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', ()=> {
+    console.log(`connected on mongoDB ${mongoose.connection.name}`)
+});
+
 
 // GET /
 app.get("/", async (req, res) => {
     res.render("index.ejs");
   });
-
-
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
